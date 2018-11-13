d3e31c3ac2a7efabd08bc76fe93bfd27c70a2e61
adrianmathys <adrianmathys@adrianscbookpro.edu.ds.fhnw.ch>
2018-11-13 10:26:39 +0100

    Update

--- views/404.php
+++ views/404.php
@@ -21,7 +21,7 @@
             <h2 class="text-center"><strong>WE-CRM | Oops!</strong></h2></div>
         <section>
             <h3 class="text-center">404<strong> Not Found</strong> </h3>
-            <p class="text-center">We couldn't find this url on our server. Maybe you'd like to try our <a href="<?php echo $GLOBALS['ROOT_URL'] ?>/login">Homepage</a> instead? </p>
+            <p class="text-center">We couldn't find this url on our server. Maybe you'd like to try our <a href="<?php echo $GLOBALS['ROOT_URL'] ?>">Homepage</a> instead? </p>
         </section>
     </div>
     <script src="assets/js/jquery.min.js"></script>
