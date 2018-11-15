15f7a20a2b76c8cf7be9aa2fa42c11b11d51d7a2
Lukas <Lukas@DESKTOP-1BU7COR.edu.ds.fhnw.ch>
2018-11-15 10:37:18 +0100

    updates

--- helpers/Validation.php
+++ helpers/Validation.php
@@ -53,8 +53,10 @@ class Validation {
      * @return boolean|string
      */
     public static function date($date){
-        if(preg_match('/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/', $date)){
-            return $date;
+        $date = date_create($date);
+        $newFormat = date_format($date, "Y-m-d");
+        if(preg_match('/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/', $newFormat)){
+            return $newFormat;
         }else{
             return false;
         }
