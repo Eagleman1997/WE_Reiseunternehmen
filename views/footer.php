dcb78e4e2fe60d1219b5f849319edf4f3f52d60e
adrianmathys <adrianmathys@adrianscbookpro.edu.ds.fhnw.ch>
2018-11-18 12:17:38 +0100

    Update

--- views/footer.php
+++ views/footer.php
@@ -8,12 +8,12 @@ use views\TemplateView;
 
 
     <div class="footer-basic">
-        <footer style="height: 50px;">
+        <footer style="height: 70px;">
             <ul class="list-inline" style="font-family: Capriola, sans-serif;">
                 <li class="list-inline-item"><a href="<?php echo $GLOBALS['ROOT_URL'] ?>" target="_top">Home&nbsp&nbsp</a></li>
                 <li class="list-inline-item"><a href="<?php echo $GLOBALS['ROOT_URL'] ?>/registration">Register</a></li>
             </ul>
-            <p data-bs-hover-animate="pulse" class="copyright" style="font-family: Capriola, sans-serif;">DreamTrips © 2018</p>
+            <p data-bs-hover-animate="pulse" class="copyright" style="font-family: Capriola, sans-serif;">DreamTrips © 2018<br>Bahnstrasse 1, 3008 Bern</p>
         </footer>
     </div>
 </body>
