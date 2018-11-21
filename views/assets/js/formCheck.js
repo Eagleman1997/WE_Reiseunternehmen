184381f2255160d41acd177557fc61c5ac6972c6
adrianmathys <adrianmathys@adrianscbookpro.edu.ds.fhnw.ch>
2018-11-21 12:18:49 +0100

    Update

--- views/assets/js/formCheck.js
+++
@@ -1,6 +0,0 @@
-function checkForm(){ 
-    if (document.registrationForm.password.value != document.registrationForm.password-repeat.value)  {
-       alert("Please repeat the password");
-       return false; 
-    }
-}
