b09142111ccfada118fa41c7892ea02b0c2c57bb
adrianmathys <adrianmathys@adrianscbookpro.edu.ds.fhnw.ch>
2018-11-22 12:17:14 +0100

    Update

--- views/adminTripTemplates.php
+++ views/adminTripTemplates.php
@@ -28,24 +28,24 @@ isset($this->tripTemplates) ? $tripTemplates = $this->tripTemplates : $tripTempl
                 <div class="container-fluid" style="background-image: url(&quot;assets/img/Mountains.jpg&quot;);background-position: center;background-size: cover;background-repeat: no-repeat;padding-bottom: 33px;"><a class="btn btn-link bg-light" role="button" href="#menu-toggle" id="menu-toggle"><i class="fa fa-bars"></i></a>
                     <h2 class="text-center" style="font-family: Capriola, sans-serif;color: #000000;"><strong>Create a new trip template.</strong></h2>
                     <div style="overflow-x: auto;">
-                    <form class="form-inline pulse animated" action="<?php echo $GLOBALS['ROOT_URL'] ?>/admin/tripTemplates" method="post" enctype="multipart/form-data" id="tripTemplateForm" style="background-color: rgba(255,255,255,0.6);margin: 20px;padding: 20px;font-family: Capriola, sans-serif;">
-                        <div class="form-group" style="margin: 10px;width: 400px;"><label class="labelsFormTripTemplates">Trip name</label><textarea class="form-control" name="name" required="" minlength="3" style="width: 400px;"></textarea></div>
-                        <div class="form-group" style="margin: 10px;width: 400px;"><label class="labelsFormTripTemplates">Description</label><textarea class="form-control" name="description" required="" minlength="3" style="width: 400px;margin-right: 0px;"></textarea></div>
-                        <div class="form-group" style="margin: 10px;width: 400px;"><label class="labelsFormTripTemplates" style="width: 400px; text-align: left; display:block;">Minimal number of participants</label><input class="form-control" type="number" name="minAllocation" value="12" required="" min="12" max="20" step="1" style="width: 100px;"></div>
-                        <div class="form-group" style="margin: 10px;width: 400px;"><label class="labelsFormTripTemplates" style="width: 400px; text-align: left; display:block;">Maximal number of participants</label><input class="form-control" type="number" name="maxAllocation" value="20" required="" min="12" max="20" step="1" style="width: 100px;"></div>
-                        <div
-                            class="form-group" style="margin: 10px;width: 400px;">
-                            <div><label class="labelsFormTripTemplates" style="width: 400px; text-align: left; display:block;">Bus</label><select class="form-control" name="busId" required="" id="busDropdownForTripTemplate" style="background-color: #ffffff;">
-                                    <optgroup label="Select bus">
-                                        <?php foreach ($buses as $bus) :  ?>
-                                        <option value="<?php echo $bus->getId();  ?>" selected=""><?php echo TemplateView::noHTML($bus->getName())." (seats: ".TemplateView::noHTML($bus->getSeats()).")"; ?></option>
-                                        <?php endforeach;  ?>
-                                    </optgroup></select></div>
-                        </div>
-                        <div class="form-group mt-auto" style="margin: 10px;width: 400px;margin-right: 2000px;"><label class="labelsFormTripTemplates" style="width: 400px; text-align: left; display:block;">Picture</label><input type="file" name="img" required="" style="background-color: transparent;"></div>
-                        <div class="form-group mt-auto"
-                             style="margin: 10px;width: 400px;"><button class="btn btn-primary" type="submit" style="width: 100px; margin-top: 20px;">Save</button></div> 
-                    </form>
+                        <form class="form-inline pulse animated" action="<?php echo $GLOBALS['ROOT_URL'] ?>/admin/tripTemplates" method="post" enctype="multipart/form-data" id="tripTemplateForm" style="background-color: rgba(255,255,255,0.6);margin: 20px;padding: 20px;font-family: Capriola, sans-serif;">
+                            <div class="form-group" style="margin: 10px;width: 400px;"><label class="labelsFormTripTemplates">Trip name</label><textarea class="form-control" name="name" required="" minlength="3" style="width: 400px;"></textarea></div>
+                            <div class="form-group" style="margin: 10px;width: 400px;"><label class="labelsFormTripTemplates">Description</label><textarea class="form-control" name="description" required="" minlength="3" style="width: 400px;margin-right: 0px;"></textarea></div>
+                            <div class="form-group" style="margin: 10px;width: 400px;"><label class="labelsFormTripTemplates" style="width: 400px; text-align: left; display:block;">Minimal number of participants</label><input class="form-control" type="number" name="minAllocation" value="12" required="" min="12" max="20" step="1" style="width: 100px;"></div>
+                            <div class="form-group" style="margin: 10px;width: 400px;"><label class="labelsFormTripTemplates" style="width: 400px; text-align: left; display:block;">Maximal number of participants</label><input class="form-control" type="number" name="maxAllocation" value="20" required="" min="12" max="20" step="1" style="width: 100px;"></div>
+                            <div
+                                class="form-group" style="margin: 10px;width: 400px;">
+                                <div><label class="labelsFormTripTemplates" style="width: 400px; text-align: left; display:block;">Bus</label><select class="form-control" name="busId" required="" id="busDropdownForTripTemplate" style="background-color: #ffffff;">
+                                        <optgroup label="Select bus">
+                                            <?php foreach ($buses as $bus) : ?>
+                                                <option value="<?php echo $bus->getId(); ?>" selected=""><?php echo TemplateView::noHTML($bus->getName()) . " (seats: " . TemplateView::noHTML($bus->getSeats()) . ")"; ?></option>
+                                            <?php endforeach; ?>
+                                        </optgroup></select></div>
+                            </div>
+                            <div class="form-group mt-auto" style="margin: 10px;width: 400px;margin-right: 2000px;"><label class="labelsFormTripTemplates" style="width: 400px; text-align: left; display:block;">Picture</label><input type="file" name="img" required="" style="background-color: transparent;"></div>
+                            <div class="form-group mt-auto"
+                                 style="margin: 10px;width: 400px;"><button class="btn btn-primary" type="submit" style="width: 100px; margin-top: 20px;">Save</button></div> 
+                        </form>
                     </div>
                     <div style="background-color: rgba(255,255,255,0.53);padding-top: 0px;margin-top: 65px;margin-bottom: 65px;padding-bottom: 20px;">
                         <h2 class="text-center" style="margin-bottom: 40px;padding: 0px;padding-top: 23px;font-family: Capriola, sans-serif;"><strong>Overview of added trip templates.</strong></h2><!DOCTYPE html>
@@ -60,49 +60,53 @@ isset($this->tripTemplates) ? $tripTemplates = $this->tripTemplates : $tripTempl
 
                                     <input class="form-control" id="tripTemplateInput" type="text" placeholder="Search trip template...">
                                     <br>
-                                    
+
+                                    <button class="btn btn-success" id="download_csv" style="margin-bottom: 10px;">Download CSV <img src="assets/img/download.png" style="width:20px; padding-left: 2px;"><span class="glyphicon glyphicon-download"></span></button>
+
                                     <div style="overflow-x: auto;">
-                                    <table id="tripTemplateOverviewTable" class="tableStyle">
-                                        <thead>
-                                            <tr>
-                                                <th>Image</th>
-                                                <th>Trip name</th>
-                                                <th>Description</th>
-                                                <th>Min. travelers</th>
-                                                <th>Max. travelers</th>
-                                                <th>From CHF</th>
-                                                <th>Bus</th>
-                                                <th>Edit</th>
-                                                <th>Delete</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody id="tripTemplateTableBody">
-                                            <?php foreach ($tripTemplates as $tripTemplate) :  ?>
-                                            <tr>
-                                                <td><img src="<?php echo TemplateView::noHTML($tripTemplate->getPicturePath()); ?>" alt="Not available" border=3 width=150></td>
-                                                <td><?php echo TemplateView::noHTML($tripTemplate->getName()); ?></td>
-                                                <td><?php echo TemplateView::noHTML($tripTemplate->getDescription()); ?></td>
-                                                <td><?php echo TemplateView::noHTML($tripTemplate->getMinAllocation()); ?></td>
-                                                <td><?php echo TemplateView::noHTML($tripTemplate->getMaxAllocation()); ?></td>
-                                                <td><?php echo TemplateView::noHTML($tripTemplate->getPrice()); ?></td>
-                                                <td><?php if($tripTemplate->getBus()){echo TemplateView::noHTML($tripTemplate->getBus()->getName())."</br>(seats: ".TemplateView::noHTML($tripTemplate->getBus()->getSeats()).")";} ?></td>
-                                                <td><a href="<?php echo $GLOBALS['ROOT_URL'] ?>/admin/tripTemplates/package/<?php echo $tripTemplate->getId(); ?>"><img src="assets/img/edit.png" alt="Edit" border=3 height=20 width=20></a></td>
-                                                <td><form id="deleteTripTemplate<?php echo $tripTemplate->getId(); ?>" action="<?php echo $GLOBALS['ROOT_URL'] ?>/admin/tripTemplates/<?php echo $tripTemplate->getId(); ?>" method="post">
-                                                        <input type="hidden" name="_method" value="DELETE"><img src="assets/img/Recycle_Bin.png" alt="Remove"  border=3 height=20 width=20 onclick="deleteHandler(<?php echo $tripTemplate->getId(); ?>)"></form></td>
-                                            </tr>
-                                            <?php endforeach;  ?>
-                                        </tbody>
-                                    </table>
+                                        <table id="tripTemplateOverviewTable" class="tableStyle">
+                                            <thead>
+                                                <tr>
+                                                    <th>Image</th>
+                                                    <th>Trip name</th>
+                                                    <th>Description</th>
+                                                    <th>Min. travelers</th>
+                                                    <th>Max. travelers</th>
+                                                    <th>From CHF</th>
+                                                    <th>Bus</th>
+                                                    <th>Edit</th>
+                                                    <th>Delete</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody id="tripTemplateTableBody">
+                                                <?php foreach ($tripTemplates as $tripTemplate) : ?>
+                                                    <tr>
+                                                        <td><img src="<?php echo TemplateView::noHTML($tripTemplate->getPicturePath()); ?>" alt="Not available" border=3 width=150></td>
+                                                        <td><?php echo TemplateView::noHTML($tripTemplate->getName()); ?></td>
+                                                        <td><?php echo TemplateView::noHTML($tripTemplate->getDescription()); ?></td>
+                                                        <td><?php echo TemplateView::noHTML($tripTemplate->getMinAllocation()); ?></td>
+                                                        <td><?php echo TemplateView::noHTML($tripTemplate->getMaxAllocation()); ?></td>
+                                                        <td><?php echo TemplateView::noHTML($tripTemplate->getPrice()); ?></td>
+                                                        <td><?php if ($tripTemplate->getBus()) {
+                                                    echo TemplateView::noHTML($tripTemplate->getBus()->getName()) . "</br>(seats: " . TemplateView::noHTML($tripTemplate->getBus()->getSeats()) . ")";
+                                                } ?></td>
+                                                        <td><a href="<?php echo $GLOBALS['ROOT_URL'] ?>/admin/tripTemplates/package/<?php echo $tripTemplate->getId(); ?>"><img src="assets/img/edit.png" alt="Edit" border=3 height=20 width=20></a></td>
+                                                        <td><form id="deleteTripTemplate<?php echo $tripTemplate->getId(); ?>" action="<?php echo $GLOBALS['ROOT_URL'] ?>/admin/tripTemplates/<?php echo $tripTemplate->getId(); ?>" method="post">
+                                                                <input type="hidden" name="_method" value="DELETE"><img src="assets/img/Recycle_Bin.png" alt="Remove"  border=3 height=20 width=20 onclick="deleteHandler(<?php echo $tripTemplate->getId(); ?>)"></form></td>
+                                                    </tr>
+<?php endforeach; ?>
+                                            </tbody>
+                                        </table>
                                     </div>
                                 </div>
 
                                 <script>
 
                                     //Remove tripTemplate
-                                    function deleteHandler(templateId){
+                                    function deleteHandler(templateId) {
                                         var c = confirm("Do you want to delete this trip template?\n\nIf you delete this trip template, trips created from it will no longer be displayed correctly.");
-                                        if(c){
-                                            $( "#deleteTripTemplate"+templateId).submit();
+                                        if (c) {
+                                            $("#deleteTripTemplate" + templateId).submit();
                                         }
                                     }
 
@@ -116,6 +120,12 @@ isset($this->tripTemplates) ? $tripTemplates = $this->tripTemplates : $tripTempl
                                         });
                                     });
 
+                                    // Allow table export in CSV format
+                                    document.querySelector("#download_csv").addEventListener("click", function () {
+                                        var html = document.querySelector("#tripTemplateOverviewTable").outerHTML;
+                                        export_table_to_csv(html, "tripTemplateOverview.csv");
+                                    });
+
                                 </script>
 
                             </body>
@@ -126,3 +136,4 @@ isset($this->tripTemplates) ? $tripTemplates = $this->tripTemplates : $tripTempl
         </div>
     </section>
     <script src="assets/js/Sidebar-Menu.js"></script>
+    <script src="assets/js/exportTable.js"></script>
--- views/adminUsers.php
+++ views/adminUsers.php
@@ -38,31 +38,36 @@ isset($this->users) ? $users = $this->users : $users = array();
 
                                 <input class="form-control" id="myInput" type="text" placeholder="Search users...">
                                 <br>
+
+                                <button class="btn btn-success" id="download_csv" style="margin-bottom: 10px;">Download CSV <img src="assets/img/download.png" style="width:20px; padding-left: 2px;"><span class="glyphicon glyphicon-download"></span></button>
+
                                 <div style="overflow-x: auto;">
-                                <table id="userAdminTable" class="tableStyle">
-                                    <thead>
-                                        <tr>
-                                            <th>Firstname</th>
-                                            <th>Lastname</th>
-                                            <th>Email</th>
-                                            <th>Admin</th>
-                                            <th>Delete</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody id="myTable">
-                                        <?php foreach ($users as $user): ?>
+                                    <table id="userAdminTable" class="tableStyle">
+                                        <thead>
                                             <tr>
-                                                <td><?php echo TemplateView::noHTML($user->getFirstname()); ?></td>
-                                                <td><?php echo TemplateView::noHTML($user->getLastname()); ?></td>
-                                                <td><?php echo TemplateView::noHTML($user->getEmail(), false); ?> </td>
-                                                <td><form id="changeRole<?php echo $user->getId(); ?>" action="<?php echo $GLOBALS['ROOT_URL'] ?>/admin/users/<?php echo $user->getId(); ?>" method="post">
-                                                        <input type="hidden" name="_method" value="PUT"><input type="checkbox" <?php if($user->getRole() == "admin"){echo 'checked';}; ?> class="adminCheckboxes"  onclick="onClickHandler(<?php echo $user->getId(); ?>)" id="userRole<?php echo $user->getId(); ?>" /></form></td>
-                                                <td><form id="deleteUser<?php echo $user->getId(); ?>" action="<?php echo $GLOBALS['ROOT_URL'] ?>/admin/users/<?php echo $user->getId(); ?>" method="post">
-                                                        <input type="hidden" name="_method" value="DELETE"><img src="assets/img/Recycle_Bin.png" alt="Remove"  border=3 height=20 width=20 onclick="deleteHandler(<?php echo $user->getId(); ?>)"></form></td>
+                                                <th>Firstname</th>
+                                                <th>Lastname</th>
+                                                <th>Email</th>
+                                                <th>Admin</th>
+                                                <th>Delete</th>
                                             </tr>
-                                        <?php endforeach; ?>
-                                    </tbody>
-                                </table>
+                                        </thead>
+                                        <tbody id="myTable">
+                                            <?php foreach ($users as $user): ?>
+                                                <tr>
+                                                    <td><?php echo TemplateView::noHTML($user->getFirstname()); ?></td>
+                                                    <td><?php echo TemplateView::noHTML($user->getLastname()); ?></td>
+                                                    <td><?php echo TemplateView::noHTML($user->getEmail(), false); ?> </td>
+                                                    <td><form id="changeRole<?php echo $user->getId(); ?>" action="<?php echo $GLOBALS['ROOT_URL'] ?>/admin/users/<?php echo $user->getId(); ?>" method="post">
+                                                            <input type="hidden" name="_method" value="PUT"><input type="checkbox" <?php if ($user->getRole() == "admin") {
+                                                echo 'checked';
+                                            }; ?> class="adminCheckboxes"  onclick="onClickHandler(<?php echo $user->getId(); ?>)" id="userRole<?php echo $user->getId(); ?>" /></form></td>
+                                                    <td><form id="deleteUser<?php echo $user->getId(); ?>" action="<?php echo $GLOBALS['ROOT_URL'] ?>/admin/users/<?php echo $user->getId(); ?>" method="post">
+                                                            <input type="hidden" name="_method" value="DELETE"><img src="assets/img/Recycle_Bin.png" alt="Remove"  border=3 height=20 width=20 onclick="deleteHandler(<?php echo $user->getId(); ?>)"></form></td>
+                                                </tr>
+<?php endforeach; ?>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
 
@@ -76,35 +81,42 @@ isset($this->users) ? $users = $this->users : $users = array();
                                         });
                                     });
                                 });
-                                
+
                                 // remove user
-                                function deleteHandler(userId){
+                                function deleteHandler(userId) {
                                     var c = confirm("Do you want to delete this user?\n\nThe user will no longer be able to log in after deletion.\n\nPlease note that it is not possible to delete the last remaining administrator.");
-                                    if(c){
-                                        $( "#deleteUser"+userId).submit();
+                                    if (c) {
+                                        $("#deleteUser" + userId).submit();
                                     }
                                 }
 
                                 // Add/remove admin right from/to users
                                 function onClickHandler(userId) {
-                                    
-                                    var id = "userRole"+userId;
+
+                                    var id = "userRole" + userId;
                                     if (!document.getElementById(id).checked) { // if the user was already admin and therefore now unchecked the checkbox
                                         document.getElementById(id).checked = true; // temporarily recheck the checkbox
                                         var c = confirm("Do you want to remove admin rights from this user?\n\nNormal user rights will be assigned to the admin.\n\nPlease note that it is not possible to remove the admin role from the last remaining administrator.");
                                         if (c) {
                                             document.getElementById(id).checked = false;
-                                            $( "#changeRole"+userId ).submit();
+                                            $("#changeRole" + userId).submit();
                                         }
                                     } else { // if the user wasn't admin yet and therefore just checked the checkbox
                                         document.getElementById(id).checked = false; // temoporarily uncheck the checkbox again
                                         var c = confirm("Do you want to assign admin rights to this user?\n\nThe user will be able to administer trip templates, trips, users, insurances, buses and hotels.");
                                         if (c) {
                                             document.getElementById(id).checked = true;
-                                            $( "#changeRole"+userId ).submit();
+                                            $("#changeRole" + userId).submit();
                                         }
                                     }
                                 }
+
+                                // Allow table export in CSV format
+                                document.querySelector("#download_csv").addEventListener("click", function () {
+                                    var html = document.querySelector("#userAdminTable").outerHTML;
+                                    export_table_to_csv(html, "userOverview.csv");
+                                });
+
 
                             </script>
 
@@ -116,3 +128,4 @@ isset($this->users) ? $users = $this->users : $users = array();
     </section>
 
     <script src="assets/js/Sidebar-Menu.js"></script>
+    <script src="assets/js/exportTable.js"></script>
