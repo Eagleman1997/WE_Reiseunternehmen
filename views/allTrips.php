8aa1bcff0adde8145f36d53eb17af078cc3c8b76
Eagleman1997 <32138456+Eagleman1997@users.noreply.github.com>
2018-12-02 18:11:04 +0100

    Update allTrips.php

--- views/allTrips.php
+++ views/allTrips.php
@@ -98,7 +98,7 @@ isset($this->trips) ? $trips = $this->trips : $trips = array();
                                     <p class="text-muted card-text"><?php echo TemplateView::noHTML($trip->getTripTemplate()->getDescription()); ?></p>
                                     <p style="padding-bottom: 0px; margin-bottom: 0px;"class="d-lg-flex justify-content-lg-end align-items-lg-end priceTag">Booked by: &nbsp<?php echo TemplateView::noHTML($trip->getUser()->getFirstName()." ".$trip->getUser()->getLastName()); ?></p>
                                     <p style="padding-bottom: 0px; margin-bottom: 20px;" class="d-lg-flex justify-content-lg-end align-items-lg-end priceTag">Departure: &nbsp<?php echo TemplateView::noHTML($trip->getDepartureDate()); ?></p>
-                                    <strong style="border-radius: 5px; padding: 5px; background-color: royalblue; color: white; border: 1px solid white;" class="priceTag">CHF &nbsp<?php echo TemplateView::noHTML($trip->getCustomerPrice()); ?></strong>
+                                    <strong style="border-radius: 5px; padding: 5px; background-color: royalblue; color: white; border: 1px solid white;" class="priceTag">CHF &nbsp<?php echo TemplateView::noHTML(number_format($trip->getCustomerPrice(),2)); ?></strong>
                                 </div>
                             </div>
                         </div>
