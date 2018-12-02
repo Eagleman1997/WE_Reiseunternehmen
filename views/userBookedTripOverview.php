2a5d11374db32fe41cb40f9a39feb475f71706fe
Eagleman1997 <32138456+Eagleman1997@users.noreply.github.com>
2018-12-02 20:18:32 +0100

    Update userBookedTripOverview.php

--- views/userBookedTripOverview.php
+++ views/userBookedTripOverview.php
@@ -37,7 +37,9 @@ if(isset($this->trip) and $trip){
     <div class="border rounded-0 register-photo" style="font-family: Capriola, sans-serif;background-size: auto;min-height: 100vh;padding-top: 0px;">
         <div style="padding-bottom: 52px;">
             <div class="container-fluid" style="margin-top: 81px;">
-                <h2 class="text-center" style="margin-bottom: 16px;"><strong>Overview of your booked trip.</strong><br></h2>
+                <h2 class="text-center" style="margin-bottom: 16px;"><strong>
+                    Overview of your booked trip "<?php echo TemplateView::noHTML($tripTemplate->getName() . "\"."); ?>
+                    </strong><br></h2>
                 <div class="scrollableDiv"><!DOCTYPE html>
                     <html lang="en">
                         <head>
